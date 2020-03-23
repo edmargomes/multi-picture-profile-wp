df9b41dadf4431ed5899dcfbc98c7029e4babd15
Edmar Gomes <edmar.gomes@nyx.tc>
2020-03-23 00:59:04 -0300

    Add the first version of the html structure in the user profile picture photo.

--- multi-picture-profile.php
+++ multi-picture-profile.php
@@ -5,8 +5,34 @@
 * Description: Add multiupload field to Picture Profile on your user profile page.
 * Author: Edmar Gomes
 * Version: 0.1
-* Requires at least: 5.5
+* Requires at least: 5.2
 * Author URI: https://github.com/edmargomes
 * Text Domain: multi-picture-profile
 * License: GPL2
 */
+
+/**
+ * Add profile field to upload images to profile.
+ * @param $user
+ */
+function eg_add_photo_profile_field($user) {
+	?>
+	<table class="form-table">
+		<tbody>
+		<tr>
+			<th>
+				<label for="eg-upload-images"><?php _e('Profile Photos', 'multi-picture-profile'); ?></label>
+			</th>
+			<td>
+				<div class="wp-media-buttons">
+					<button class="button" id="eg-upload-images"><?php _e('Add', 'multi-picture-profile'); ?></button>
+					<button class="button"><?php _e('Remove', 'multi-picture-profile'); ?></button>
+				</div>
+			</td>
+		</tr>
+		</tbody>
+	</table>
+	<?php
+}
+add_action( 'show_user_profile', 'eg_add_photo_profile_field' );
+add_action( 'edit_user_profile', 'eg_add_photo_profile_field' );
