dc115f410032fac3ef8ac1a045011d1ac0f53142
Edmar Gomes <edmar.gomes@nyx.tc>
2020-03-23 00:27:09 -0300

    Add start structure to the plugin.

---
+++ multi-picture-profile.php
@@ -0,0 +1,12 @@
+<?php
+/*
+* Plugin Name: Multi Picture Profile
+* Plugin URI: https://github.com/edmargomes/multi-picture-profile-wp
+* Description: Add multiupload field to Picture Profile on your user profile page.
+* Author: Edmar Gomes
+* Version: 0.1
+* Requires at least: 5.5
+* Author URI: https://github.com/edmargomes
+* Text Domain: multi-picture-profile
+* License: GPL2
+*/
